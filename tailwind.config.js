486da6f254b3be5e3167b766d28da9fc89a53ba3
ccccchriz <76066995+ccccchriz@users.noreply.github.com>
2024-04-02 18:43:42 +0200

    added default styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,37 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    fontFamily: {
+      primary: ["Jost*", "sans-serif"],
+    },
+    fontWeight: {
+      regular: 400,
+      medium: 500,
+    },
+    fontSize: {
+      "heading-s": "1.625rem",
+      "heading-m": "2.5rem",
+      "heading-l": "3rem",
+      "mobile-heading-s": "1.25rem",
+      "mobile-heading-m": "1.75rem",
+      "mobile-heading-l": "2rem",
+      body: "1rem",
+      "body-sm": "0.9375rem",
+      link: "0.875rem",
+      "mobile-link": "1.5rem",
+    },
+    colors: {
+      peach: "hsl(11, 73%, 66%)",
+      "light-peach": "hsl(11, 100%, 80%)",
+      white: "hsl(0, 0%, 100%)",
+      "light-gray": "hsl(210, 17%, 95%)",
+      "dark-gray": "hsl(264, 5%, 20%)",
+      black: "hsl(270, 3%, 11%)",
+    },
+    screens: {
+      tablet: "34rem",
+      desktop: "54rem",
+    },
   },
   plugins: [],
 };
