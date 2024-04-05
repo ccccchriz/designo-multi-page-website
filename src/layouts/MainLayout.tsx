10fa0f4d279e02c994be74e82c2c8a5995032501
ccccchriz <76066995+ccccchriz@users.noreply.github.com>
2024-04-05 18:41:20 +0200

    added h1

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -9,7 +9,10 @@ export default function MainLayout({ children }: MainLayoutProps) {
   return (
     <div className="flex flex-col content-center min-h-svh">
       <Header></Header>
-      <main className="w-full max-w-site grow ">{children}</main>
+      <main className="w-full max-w-site grow ">
+        <h1 className="sr-only">Designo</h1>
+        {children}
+      </main>
       <Footer></Footer>
     </div>
   );
