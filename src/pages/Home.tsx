c1a3fd00ffd96be27061dde769fea7cef8c0ba22
ccccchriz <76066995+ccccchriz@users.noreply.github.com>
2024-04-07 16:11:55 +0200

    added home styling

---
+++ src/pages/Home.tsx
@@ -0,0 +1,25 @@
+import phone from "../assets/home/desktop/image-hero-phone.png";
+
+export default function Home() {
+  return (
+    <>
+      <div className="bg-peach px-6 py-20 text-white text-center grid gap-4 overflow-hidden tablet:mx-6 tablet:rounded-2xl justify-items-center bg-[url('/images/home/bg-pattern-hero-home.svg')] bg-no-repeat bg-[right_75%_center]">
+        <h2 className="text-mobile-heading-l tablet:text-heading-l max-w-[35.8125rem] leading-none font-medium">
+          Award-winning custom designs and digital branding solutions
+        </h2>
+        <p className="text-body-sm tablet:text-body max-w-[27.8125rem]">
+          With over 10 years in the industry, we are experienced in creating
+          fully responsive websites, app design, and engaging brand experiences.
+          Find out more about our services.
+        </p>
+        <a
+          href=""
+          className="bg-white text-black uppercase py-4 px-6 tracking-[2px] w-max font-medium rounded-lg"
+        >
+          Learn More
+        </a>
+        <img src={phone} alt="" className="-mb-80 scale-150 mt-16" />
+      </div>
+    </>
+  );
+}
