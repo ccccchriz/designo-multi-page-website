e5a2d4b863b24cad053331313344b613106ad727
ccccchriz <76066995+ccccchriz@users.noreply.github.com>
2024-04-15 09:30:50 +0200

    added tablet text styling

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -21,8 +21,10 @@ export default function About() {
           <img src={heroMobile} alt="" className="object-cover w-full" />
         </picture>
         <div className="bg-peach px-6 py-20 text-white text-center grid gap-6">
-          <h2 className="font-medium text-mobile-heading-l">About Us</h2>
-          <p className="text-body-sm">
+          <h2 className="font-medium text-mobile-heading-l tablet:text-heading-l">
+            About Us
+          </h2>
+          <p className="text-body-sm tablet:text-body">
             Founded in 2010, we are a creative agency that produces lasting
             results for our clients. We’ve partnered with many startups,
             corporations, and nonprofits alike to craft designs that make real
@@ -38,16 +40,16 @@ export default function About() {
           <img src={talentMobile} alt="" className="object-cover w-full" />
         </picture>
         <div className="bg-light-peach px-6 py-20 text-dark-gray text-center grid gap-6 bg-opacity-20">
-          <h2 className="font-medium text-mobile-heading-l text-peach">
+          <h2 className="font-medium text-mobile-heading-l tablet:text-heading-m text-peach">
             World-class talent
           </h2>
-          <p className="text-body-sm">
+          <p className="text-body-sm tablet:text-body">
             We are a crew of strategists, problem-solvers, and technologists.
             Every design is thoughtfully crafted from concept to launch,
             ensuring success in its given market. We are constantly updating our
             skills in a myriad of platforms.
           </p>
-          <p className="text-body-sm">
+          <p className="text-body-sm tablet:text-body">
             Our team is multi-disciplinary and we are not merely interested in
             form — content and meaning are just as important. We give great
             importance to craftsmanship, service, and prompt delivery. Clients
@@ -98,17 +100,17 @@ export default function About() {
           <img src={realMobile} alt="" className="object-cover w-full" />
         </picture>
         <div className="bg-light-peach px-6 py-20 text-dark-gray text-center grid gap-6 bg-opacity-20">
-          <h2 className="font-medium text-mobile-heading-l text-peach">
+          <h2 className="font-medium text-mobile-heading-l tablet:text-heading-m text-peach">
             The real deal
           </h2>
-          <p className="text-body-sm">
+          <p className="text-body-sm tablet:text-body">
             As strategic partners in our clients’ businesses, we are ready to
             take on any challenge as our own. Solving real problems require
             empathy and collaboration, and we strive to bring a fresh
             perspective to every opportunity. We make design and technology more
             accessible and give you tools to measure success.
           </p>
-          <p className="text-body-sm">
+          <p className="text-body-sm tablet:text-body">
             We are visual storytellers in appealing and captivating ways. By
             combining business and marketing strategies, we inspire audiences to
             take action and drive real results.
