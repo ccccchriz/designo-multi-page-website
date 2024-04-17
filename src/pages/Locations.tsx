e95c9d82a12283cff68f6c532f872ad09c036de7
ccccchriz <76066995+ccccchriz@users.noreply.github.com>
2024-04-17 11:50:09 +0200

    tablet styling

--- src/pages/Locations.tsx
+++ src/pages/Locations.tsx
@@ -4,8 +4,8 @@ export default function Locations() {
   return (
     <div>
       <h2 className="sr-only">Our Locations</h2>
-      <ul className="grid gap-6 pb-6">
-        <li>
+      <ul className="grid gap-6 tablet:gap-28 pb-6 tablet:pb-28 tablet:mx-6">
+        <li className="grid tablet:gap-8">
           <MapContainer
             className="h-[20rem]"
             center={[43.64099990951995, -79.41015585824242]}
@@ -18,8 +18,8 @@ export default function Locations() {
             />
             <Marker position={[43.64099990951995, -79.41015585824242]}></Marker>
           </MapContainer>
-          <div className="bg-light-peach bg-opacity-20 py-20 px-6 grid place-content-center gap-6 text-center text-body-sm">
-            <h3 className="text-mobile-heading-l font-medium text-peach">
+          <div className="bg-light-peach bg-opacity-20 py-20 px-6 grid place-content-center gap-6 text-center text-body-sm tablet:rounded-2xl tablet:grid-cols-2 tablet:place-items-start tablet:px-20">
+            <h3 className="text-mobile-heading-l tablet:text-heading-m font-medium text-peach tablet:col-[1/3]">
               Canada
             </h3>
             <p className="grid">
@@ -33,7 +33,7 @@ export default function Locations() {
             </p>
           </div>
         </li>
-        <li>
+        <li className="grid tablet:gap-8">
           <MapContainer
             className="h-[20rem]"
             center={[-31.732751704358066, 152.6971584824212]}
@@ -48,8 +48,8 @@ export default function Locations() {
               position={[-31.732751704358066, 152.6971584824212]}
             ></Marker>
           </MapContainer>
-          <div className="bg-light-peach bg-opacity-20 py-20 px-6 grid place-content-center gap-6 text-center text-body-sm">
-            <h3 className="text-mobile-heading-l font-medium text-peach">
+          <div className="bg-light-peach bg-opacity-20 py-20 px-6 grid place-content-center gap-6 text-center text-body-sm tablet:rounded-2xl tablet:grid-cols-2 tablet:place-items-start tablet:px-20">
+            <h3 className="text-mobile-heading-l tablet:text-heading-m font-medium text-peach tablet:col-[1/3]">
               Australia
             </h3>
 
@@ -65,7 +65,7 @@ export default function Locations() {
             </p>
           </div>
         </li>
-        <li>
+        <li className="grid tablet:gap-8">
           <MapContainer
             className="h-[20rem]"
             center={[51.72720237281599, -3.862572568728834]}
@@ -78,8 +78,8 @@ export default function Locations() {
             />
             <Marker position={[51.72720237281599, -3.862572568728834]}></Marker>
           </MapContainer>
-          <div className="bg-light-peach bg-opacity-20 py-20 px-6 grid place-content-center gap-6 text-center text-body-sm">
-            <h3 className="text-mobile-heading-l font-medium text-peach">
+          <div className="bg-light-peach bg-opacity-20 py-20 px-6 grid place-content-center gap-6 text-center text-body-sm tablet:rounded-2xl tablet:grid-cols-2 tablet:place-items-start tablet:px-20">
+            <h3 className="text-mobile-heading-l tablet:text-heading-m font-medium text-peach tablet:col-[1/3]">
               United Kingdom
             </h3>
             <p className="grid">
