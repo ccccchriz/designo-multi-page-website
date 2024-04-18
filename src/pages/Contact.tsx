422f143225581ab7470e37e12d9745289a48a892
ccccchriz <76066995+ccccchriz@users.noreply.github.com>
2024-04-18 12:16:50 +0200

    started contact page

---
+++ src/pages/Contact.tsx
@@ -0,0 +1,21 @@
+export default function Contact() {
+  return (
+    <>
+      <div className="">
+        <h2>Contact Us</h2>
+        <p>
+          Ready to take it to the next level? Let’s talk about your project or
+          idea and find out how we can help your business grow. If you are
+          looking for unique digital experiences that’s relatable to your users,
+          drop us a line.
+        </p>
+        <form action="">
+          <input type="text" name="" id="" />
+          <input type="email" name="" id="" />
+          <input type="tel" name="" id="" />
+          <textarea name="" id=""></textarea>
+        </form>
+      </div>
+    </>
+  );
+}
