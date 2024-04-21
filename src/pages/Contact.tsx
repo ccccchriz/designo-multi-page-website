bb132b179b6d1741f8a1757842400aa192747d92
ccccchriz <76066995+ccccchriz@users.noreply.github.com>
2024-04-21 19:27:12 +0200

    added some html stuff

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,7 +1,7 @@
 export default function Contact() {
   return (
     <>
-      <div className="">
+      <div className="bg-peach text-white">
         <h2>Contact Us</h2>
         <p>
           Ready to take it to the next level? Let’s talk about your project or
@@ -10,10 +10,31 @@ export default function Contact() {
           drop us a line.
         </p>
         <form action="">
-          <input type="text" name="" id="" />
-          <input type="email" name="" id="" />
-          <input type="tel" name="" id="" />
-          <textarea name="" id=""></textarea>
+          <label htmlFor="name" className="sr-only">
+            Name
+          </label>
+          <input type="text" name="name" id="name" placeholder="Name" />
+          <label htmlFor="email" className="sr-only">
+            Email Address
+          </label>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            placeholder="Email Address"
+          />
+          <label htmlFor="phone" className="sr-only">
+            Phone
+          </label>
+          <input type="tel" name="phone" id="phone" placeholder="Phone" />
+          <label htmlFor="message" className="sr-only">
+            Your Message
+          </label>
+          <textarea
+            name="message"
+            id="message"
+            placeholder="Your Message"
+          ></textarea>
           <button type="submit">Submit</button>
         </form>
       </div>
