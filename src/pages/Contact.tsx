9741889931eb8b9c388ed0958e84a9a086b11167
ccccchriz <76066995+ccccchriz@users.noreply.github.com>
2024-04-22 18:30:20 +0200

    styling

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,7 +1,7 @@
 export default function Contact() {
   return (
     <>
-      <div className="bg-peach text-white">
+      <div className="bg-peach text-white grid p-6">
         <h2>Contact Us</h2>
         <p>
           Ready to take it to the next level? Let’s talk about your project or
@@ -9,11 +9,17 @@ export default function Contact() {
           looking for unique digital experiences that’s relatable to your users,
           drop us a line.
         </p>
-        <form action="">
+        <form action="" className="grid">
           <label htmlFor="name" className="sr-only">
             Name
           </label>
-          <input type="text" name="name" id="name" placeholder="Name" />
+          <input
+            type="text"
+            name="name"
+            id="name"
+            placeholder="Name"
+            className="bg-[transparent] border-b border-b-white p-3 placeholder:text-white placeholder:opacity-15 font-medium"
+          />
           <label htmlFor="email" className="sr-only">
             Email Address
           </label>
@@ -22,11 +28,18 @@ export default function Contact() {
             name="email"
             id="email"
             placeholder="Email Address"
+            className="bg-[transparent] border-b border-b-white p-3 placeholder:text-white placeholder:opacity-15 font-medium"
           />
           <label htmlFor="phone" className="sr-only">
             Phone
           </label>
-          <input type="tel" name="phone" id="phone" placeholder="Phone" />
+          <input
+            type="tel"
+            name="phone"
+            id="phone"
+            placeholder="Phone"
+            className="bg-[transparent] border-b border-b-white p-3 placeholder:text-white placeholder:opacity-15 font-medium"
+          />
           <label htmlFor="message" className="sr-only">
             Your Message
           </label>
@@ -34,6 +47,7 @@ export default function Contact() {
             name="message"
             id="message"
             placeholder="Your Message"
+            className="bg-[transparent] border-b border-b-white p-3 placeholder:text-white placeholder:opacity-15 font-medium"
           ></textarea>
           <button type="submit">Submit</button>
         </form>
